acbbd4f501605dbc6115bb9d18907c610d7836ca
Farrukh Ahmed <92082052+Farrukh-Ahmed1@users.noreply.github.com>
2023-06-21 17:00:39 +0500

    All Done

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Fragment, useState } from "react";
 import Image from "next/image";
 import { Combobox, Transition } from "@headlessui/react";
--- utils/index.ts
+++ utils/index.ts
@@ -66,7 +66,7 @@ export async function fetchCars(filters: FilterProps) {
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
   const { make, model, year } = car;
-  url.searchParams.append("customer", process.env.NEXT_PUBLIC_IMAGIN_STUDIO_CUSTOMER || '');
+  url.searchParams.append("customer", "hrjavascript-mastery");
   url.searchParams.append("make", make);
   url.searchParams.append("modelFamily", model.split(" ")[0]);
   url.searchParams.append("zoomType", "fullscreen");
