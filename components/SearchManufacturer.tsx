0326d05ad0495017243483cce82450a685295a44
Farrukh Ahmed <92082052+Farrukh-Ahmed1@users.noreply.github.com>
2023-06-21 16:56:01 +0500

    Update SearchManufacturer.tsx

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import { Fragment, useState } from "react";
+import Image from "next/image";
 import { Combobox, Transition } from "@headlessui/react";
 import { manufacturers } from "@/constants";
 import { SearchManufacturerProps } from "@/types";
