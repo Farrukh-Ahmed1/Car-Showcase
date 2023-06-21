40251411420680f1a6e5253dd79757c5562a25b3
Farrukh Ahmed <92082052+Farrukh-Ahmed1@users.noreply.github.com>
2023-06-21 16:31:07 +0500

    All done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,25 @@
-import { Footer, Navbar } from '@/components'
-import './globals.css'
+import { Footer, Navbar } from '@/components';
+import Head from 'next/head';
+import './globals.css';
 
 export const metadata = {
   title: 'Car Hub',
-  description: 'Discover the best cars is the world.',
-}
+  description: 'Discover the best cars in the world.',
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content={metadata.description} />
+        <title>{metadata.title}</title>
+      </Head>
       <body className="relative">
         <Navbar />
         {children}
         <Footer />
       </body>
     </html>
-  )
+  );
 }
